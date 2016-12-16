33bb9a0343612ad154ac589a3534c01a6d7562a8
hasna <hasnahasnakgm@gmail.com>
2016-12-17 00:58:22 +0530

    un commented test function for select_word()

--- test_hangman.py
+++ test_hangman.py
@@ -1,24 +1,24 @@
 import hangman
 
-# def test_select_word_avoid_bad():
-# 	fname = "/tmp/test_dict.txt"
-# 	f = open(fname, "w")
-# 	f.writelines(["aa\n","Cccccccc\n","ewewq\n"])
-# 	f.close()
+def test_select_word_avoid_bad():
+	fname = "/tmp/test_dict.txt"
+	f = open(fname, "w")
+	f.writelines(["aa\n","Cccccccc\n","ewewq\n"])
+	f.close()
 
-# 	word = hangman.select_word(fname)
+	word = hangman.select_word(fname)
 
-# 	assert word == ''
+	assert word == ''
 
-# def test_select_word_select_good():
-# 	fname = "/tmp/test_dict.txt"
-# 	f = open(fname, "w")
-# 	f.writelines(["aaa\n","Cccccccc\n","fg'ewewq\n","testabcdas\n"])
-# 	f.close()
+def test_select_word_select_good():
+	fname = "/tmp/test_dict.txt"
+	f = open(fname, "w")
+	f.writelines(["aaa\n","Cccccccc\n","fg'ewewq\n","testabcdas\n"])
+	f.close()
 
-# 	word = hangman.select_word(fname)
+	word = hangman.select_word(fname)
 
-# 	assert word == 'testabcdas'
+	assert word == 'testabcdas'
 
 def test_check_letter_exist_good():
     word = "word"
@@ -43,3 +43,12 @@ def test_replace_letters_bad():
 	word = "aabbccaabbcc"
 	secretword = hangman.replace_letters(word, correctGuesses)
 	assert secretword == "************"
+
+def test_check_word_found_good()	:
+	word = "afssgsg"
+	assert hangman.check_word_found(word) == True
+
+def test_check_word_found_bad()	:
+	word = "afs**gsg"
+	assert hangman.check_word_found(word) == False
+	assert hangman.check_word_found('*****') == False
